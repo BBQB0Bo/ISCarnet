03b53d636d31b28f200350b3923adcdb0d163112
CDU55 <48677296+CDU55@users.noreply.github.com>
2020-01-02 22:54:43 +0200

    Add Exam Validation

---
+++ ISCarnet/ClassLibrary1/FutureDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataBaseLibrary
+{
+    public class FutureDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime examDate = DateTime.Parse(value.ToString());
+            DateTime today = DateTime.Today;
+            if (today > examDate)
+            {
+                var result = new ValidationResult("The exam date must be in the future!");
+                return result;
+            }
+            return null;
+
+        }
+    }
+}
