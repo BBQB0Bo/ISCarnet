3eb662428043c15796eb963ff9583cd5da1df74b
Robert <48147318+BBQB0Bo@users.noreply.github.com>
2020-01-13 00:04:21 +0200

    Puncte harta

--- ISCarnet/IS_CARNET/wwwroot/js/map.js
+++ ISCarnet/IS_CARNET/wwwroot/js/map.js
@@ -1,7 +1,6 @@
 var map;
 
 function initMap() {
-
     //Map options and new map 
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 47.168733, lng: 27.541590 },
@@ -9,33 +8,11 @@ function initMap() {
     });
 }
 
-function addMarker() {
-
-    var x = document.getElementById("punctPlecare").value;
-
-    /*Trebuie modificate coordonatele si adaugate zone noi*/
-
-    if (x == "Zona Gara") {
-        coords = { lat: 47.168731, lng: 27.541580 };
-    } else if (x == "Moara de Foc") {
-        coords = { lat: 47.128730, lng: 27.541 };
-    }
-
+function addMarker(latitudine, longitudine) {
+    coords = { lat: latitudine, lng: longitudine }
     var marker = new google.maps.Marker({
         position: coords,
         map: map,
         icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    });
-
-    map.addListener('center_changed', function () {
-    
-        window.setTimeout(function () {
-            map.panTo(marker.getPosition());
-        }, 3000);
-    });
-
-    marker.addListener('click', function () {
-        map.setZoom(15);
-        map.setCenter(marker.getPosition());
     });
 }
